55326189f85673953dc60c986add7fe2d679557b
ученик <dmitta>
2021-02-08 17:58:56 +0500

    sadacha kotoruu ia ne budu delat'

--- main.py
+++ main.py
@@ -1,31 +1,19 @@
+import os
 import sys
+
 import pygame
+import requests
 
-pygame.init()
+map_request = "http://static-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b"
+response = requests.get(map_request)
 
-width = 50
-height = 500
-
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('YAHOOOO')
-clock = pygame.time.Clock()
-
-x = 30
-y = 30
-vx = 50
-vy = 50
-
-while True:
-    dt = clock.tick(50) / 1000.0
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT or event.type == pygame.KEYDOWN:
-            sys.exit()
-
-    x += vx * dt
-    y += vy * dt
-
-    screen.fill((0, 0, 0))
-    pygame.draw.circle(screen, (150, 10, 50), (int(x), int(y)), 20)
-
-    pygame.display.flip()
+if response:
+    json_response = response.json()
+    toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+    toponym_address = toponym["metaDataProperty"]["GeocoderMetaData"]["text"]
+    toponym_coodrinates = toponym["Point"]["pos"]
+    print(toponym_address, "имеет координаты:", toponym_coodrinates)
+else:
+    print("Ошибка выполнения запроса:")
+    print(map_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
